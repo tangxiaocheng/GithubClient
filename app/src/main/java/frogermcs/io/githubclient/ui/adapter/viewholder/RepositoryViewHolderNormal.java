d7e286e7c2c2bfba189adae94cae3e796e77ab35
Randy <tangdengcheng@gmail.com>
2021-01-12 17:24:49 -0800

    Remove AutoFactory library from project dependencies. Run

--- app/src/main/java/frogermcs/io/githubclient/ui/adapter/viewholder/RepositoryViewHolderNormal.java
+++ app/src/main/java/frogermcs/io/githubclient/ui/adapter/viewholder/RepositoryViewHolderNormal.java
@@ -4,7 +4,6 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.auto.factory.AutoFactory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
